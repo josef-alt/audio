05b822524d75e520d36041a09be18ebd37a33bbc
trawnlegacy <30385688+trawnlegacy@users.noreply.github.com>
2024-09-17 15:53:49 -0500

    added getMetadata method to AudioFile

--- src/main/java/org/audio/AudioFile.java
+++ src/main/java/org/audio/AudioFile.java
@@ -2,6 +2,7 @@ package org.audio;
 import java.nio.file.Path;
 import java.util.Map;
 
+import org.audio.metadata.ID3TagReader;
 import org.audio.utils.FileUtils;
 import org.audio.utils.FileUtils.Format;
 
@@ -11,5 +12,19 @@ public class AudioFile {
 
 	public AudioFile(Path src) {
 		source = src;
+
+		// TODO new thread
+		if (FileUtils.determineFormatByHeader(source) == Format.MP3) {
+			metadata = ID3TagReader.getMetadata(source);
+		}
+	}
+
+	/**
+	 * Retrieve metadata from {@code source} as key-value pairs
+	 * 
+	 * @return An unmodifiable map containing metadata
+	 */
+	public Map<String, String> getMetadata() {
+		return metadata;
 	}
 }

fec8eaf5f0961cc3544c70d383ba343c8d2b753d
trawnlegacy <30385688+trawnlegacy@users.noreply.github.com>
2024-09-29 17:28:46 -0500

    added vorbis tag reading

--- src/main/java/org/audio/metadata/FLACReader.java
+++ src/main/java/org/audio/metadata/FLACReader.java
@@ -2,6 +2,7 @@ package org.audio.metadata;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.nio.channels.FileChannel;
 import java.nio.file.StandardOpenOption;
 import java.util.Collections;
@@ -75,6 +76,57 @@ public class FLACReader extends MetadataReader {
 					// SEEKTABLE
 				} else if (blockType == 4) {
 					// VORBIS_COMMENT
+					// steps taken directly from https://xiph.org/vorbis/doc/v-comment.html
+
+					// vorbis uses little endian
+					buffer.order(ByteOrder.LITTLE_ENDIAN);
+
+					// 1) [vendor_length] = read an unsigned integer of 32 bits
+					int vendor_length = buffer.getInt();
+
+					// 2) [vendor_string] = read a UTF-8 vector as [vendor_length] octets
+					// because vorbis uses utf-8, endianness shouldn't matter here
+					byte[] vendor_string = new byte[vendor_length];
+					buffer.get(vendor_string);
+
+					// 3) [user_comment_list_length] = read an unsigned integer of 32 bits
+					int user_comment_list_length = buffer.getInt();
+
+					// 4) iterate [user_comment_list_length] times {
+					for (int c = 0; c < user_comment_list_length; ++c) {
+						// 5) [length] = read an unsigned integer of 32 bits
+						int length = buffer.getInt();
+
+						// 6) this iteration's user comment = read a UTF-8 vector as [length] octets
+						byte[] comment = new byte[length];
+						buffer.get(comment);
+
+						int equalSign = -1;
+						for (int i = 0; i < length; ++i) {
+							if (comment[i] == '=') {
+								equalSign = i;
+								break;
+							}
+						}
+
+						if (equalSign != -1) {
+							String tag = new String(comment, 0, equalSign);
+							String value = new String(comment, equalSign + 1, length - equalSign - 1);
+
+							// convert vorbis tags to a descriptor common across all supported formats
+							if (VORBIS_TAGS.containsKey(tag)) {
+								tag = VORBIS_TAGS.get(tag);
+							}
+
+							metadata.addTextField(tag, value);
+						}
+					}
+
+					// steps 7-9 are not applicable to FLAC as framing_bit is not used
+
+					// explicit return to big endian even though buffer will be reassigned
+					buffer.order(ByteOrder.BIG_ENDIAN);
+
 				} else if (blockType == 5) {
 					// CUESHEET
 				} else if (blockType == 6) {

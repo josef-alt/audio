4cc856647f46c9ff50461f62a5b0160ef39339b6
trawnlegacy <30385688+trawnlegacy@users.noreply.github.com>
2024-10-23 18:30:17 -0500

    created constants class to standardize tag names

---
+++ src/main/java/org/audio/metadata/Constants.java
@@ -0,0 +1,108 @@
+package org.audio.metadata;
+
+/**
+ * This class serves to provide a common value for the metadata tag names
+ */
+public final class Constants {
+
+	/**
+	 * This class is for constants and need not be instantiated
+	 */
+	private Constants() {
+	};
+
+	/**
+	 * Name of the artist or lead performer
+	 */
+	public static final String ARTIST_NAME = "Artist";
+
+	/**
+	 * Name of the album
+	 */
+	public static final String ALBUM_NAME = "Album";
+
+	/**
+	 * Name of the individual piece
+	 */
+	public static final String TITLE = "Title";
+
+	/**
+	 * Name of the original composer
+	 */
+	public static final String COMPOSER = "Composer";
+
+	/**
+	 * Name of the orchestra conductor
+	 */
+	public static final String CONDUCTOR = "Conductor";
+
+	/**
+	 * Name of the orchestra or backing musicians
+	 */
+	public static final String ACCOMPANIMENT = "Accompaniment";
+
+	/**
+	 * Content type or genre
+	 */
+	public static final String GENRE = "Genre";
+
+	/**
+	 * Year of publication or recording. Should be a single integer.
+	 */
+	public static final String YEAR = "Year";
+
+	/**
+	 * Date of publication or recording
+	 */
+	public static final String DATE = "Date";
+
+	/**
+	 * Disc number if multiple parts
+	 */
+	public static final String DISC_NUMBER = "Disc Number";
+
+	/**
+	 * Track number or album position
+	 */
+	public static final String TRACK_NUMBER = "Track Number";
+
+	/**
+	 * Copyright information
+	 */
+	public static final String COPYRIGHT = "Copyright";
+
+	/*
+	 * International Standard Recording Code
+	 */
+	public static final String ISRC = "ISRC";
+
+	/**
+	 * Software/Hardware settings used for encoding
+	 */
+	public static final String ENCODING_INFO = "Encoding Software/Hardware";
+
+	/**
+	 * Publisher
+	 */
+	public static final String PUBLISHER = "Publisher";
+
+	/*
+	 * Official artist webpage
+	 */
+	public static final String ARTIST_WEBPAGE = "Artist Site";
+
+	/**
+	 * Official album webpage
+	 */
+	public static final String ALBUM_WEBPAGE = "Album Site";
+
+	/**
+	 * Official audio webpage
+	 */
+	public static final String FILE_WEBPAGE = "File Site";
+
+	/**
+	 * Official publisher webpage
+	 */
+	public static final String PUBLISHER_WEBPAGE = "Publisher Site";
+}

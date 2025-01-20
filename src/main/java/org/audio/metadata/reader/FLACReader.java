644296335590249a8179b3d90ca7fa9e871b5a04
trawnlegacy <30385688+trawnlegacy@users.noreply.github.com>
2025-01-20 14:58:29 -0600

    extracted vorbis comment and image extraction logic to functions

--- src/main/java/org/audio/metadata/reader/FLACReader.java
+++ src/main/java/org/audio/metadata/reader/FLACReader.java
@@ -87,96 +87,23 @@ public class FLACReader extends MetadataReader {
 				int blockType = flags & 0x7F;
 				int blockLength = buffer.getInt(0) & 0xFFFFFF;
 
-				buffer = ByteBuffer.allocate(blockLength);
-				channel.read(buffer);
-				buffer.flip();
-
-				if (blockType == 0) {
-					// STREAMINFO
-				} else if (blockType == 1) {
-					// PADDING
-				} else if (blockType == 2) {
-					// APPLICATION
-				} else if (blockType == 3) {
-					// SEEKTABLE
-				} else if (blockType == 4) {
-					// VORBIS_COMMENT
-					// steps taken directly from https://xiph.org/vorbis/doc/v-comment.html
-
-					// vorbis uses little endian
-					buffer.order(ByteOrder.LITTLE_ENDIAN);
-
-					// 1) [vendor_length] = read an unsigned integer of 32 bits
-					int vendor_length = buffer.getInt();
-
-					// 2) [vendor_string] = read a UTF-8 vector as [vendor_length] octets
-					// because vorbis uses utf-8, endianness shouldn't matter here
-					byte[] vendor_string = new byte[vendor_length];
-					buffer.get(vendor_string);
-
-					// 3) [user_comment_list_length] = read an unsigned integer of 32 bits
-					int user_comment_list_length = buffer.getInt();
-
-					// 4) iterate [user_comment_list_length] times {
-					for (int c = 0; c < user_comment_list_length; ++c) {
-						// 5) [length] = read an unsigned integer of 32 bits
-						int length = buffer.getInt();
-
-						// 6) this iteration's user comment = read a UTF-8 vector as [length] octets
-						byte[] comment = new byte[length];
-						buffer.get(comment);
-
-						int equalSign = -1;
-						for (int i = 0; i < length; ++i) {
-							if (comment[i] == '=') {
-								equalSign = i;
-								break;
-							}
-						}
-
-						if (equalSign != -1) {
-							String tag = new String(comment, 0, equalSign);
-							String value = new String(comment, equalSign + 1, length - equalSign - 1);
-
-							// convert vorbis tags to a descriptor common across all supported formats
-							if (VORBIS_TAGS.containsKey(tag)) {
-								tag = VORBIS_TAGS.get(tag);
-							}
-
-							metadata.addTextField(tag, value);
-						}
-					}
-
-					// steps 7-9 are not applicable to FLAC as framing_bit is not used
-
-					// explicit return to big endian even though buffer will be reassigned
-					buffer.order(ByteOrder.BIG_ENDIAN);
-
-				} else if (blockType == 5) {
-					// CUESHEET
+				/**
+				 * Block Types:
+				 * 0 - STREAMINFO
+				 * 1 - PADDING
+				 * 2 - APPLICATION
+				 * 3 - SEEKTABLE
+				 * 4 - VORBIS_COMMENT
+				 * 5 - CUESHEET
+				 * 6 - PICTURE
+				 */
+				if (blockType == 4) {
+					extractVORBISData(channel, blockLength, metadata);
 				} else if (blockType == 6) {
-					// PICTURE
-
-					int pictureType = buffer.getInt();
-					int mimeLength = buffer.getInt();
-					byte[] mimeType = new byte[mimeLength];
-					buffer.get(mimeType);
-
-					int descriptionLength = buffer.getInt();
-					byte[] description = new byte[descriptionLength];
-					buffer.get(description);
-
-					int imageWidth = buffer.getInt();
-					int imageHeight = buffer.getInt();
-
-					int colorDepth = buffer.getInt();
-					int numColors = buffer.getInt();
-					int imageLength = buffer.getInt();
-					byte[] image = new byte[imageLength];
-					buffer.get(image);
-
-					CoverArt cover = new CoverArt(new String(mimeType), image);
-					metadata.addImage(cover);
+					extractImage(channel, blockLength, metadata);
+				} else {
+					// skip over other blocks for now
+					channel.position(channel.position() + blockLength);
 				}
 			} while (!lastBlock);
 
@@ -186,5 +113,114 @@ public class FLACReader extends MetadataReader {
 		}
 
 		return metadata;
+	}
+
+	/**
+	 * Util function for extracting images from a FLAC {@link FileChannel}.
+	 * 
+	 * @param channel     input channel
+	 * @param blockLength number of bytes to read
+	 * @param metadata    instance to be populated with extracted images
+	 */
+	public static void extractImage(FileChannel channel, int blockLength, Metadata metadata) {
+		try {
+			ByteBuffer buffer = ByteBuffer.allocate(blockLength);
+			channel.read(buffer);
+			buffer.flip();
+
+			int pictureType = buffer.getInt();
+			int mimeLength = buffer.getInt();
+			byte[] mimeType = new byte[mimeLength];
+			buffer.get(mimeType);
+
+			int descriptionLength = buffer.getInt();
+			byte[] description = new byte[descriptionLength];
+			buffer.get(description);
+
+			int imageWidth = buffer.getInt();
+			int imageHeight = buffer.getInt();
+
+			int colorDepth = buffer.getInt();
+			int numColors = buffer.getInt();
+			int imageLength = buffer.getInt();
+			byte[] image = new byte[imageLength];
+			buffer.get(image);
+
+			CoverArt cover = new CoverArt(new String(mimeType), image);
+			metadata.addImage(cover);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Util function for extracting VORBIS comments from a {@link FileChannel}.
+	 * 
+	 * @param channel     input channel
+	 * @param blockLength number of bytes to read
+	 * @param metadata    instance to be populated with extracted data
+	 */
+	public static void extractVORBISData(FileChannel channel, int blockLength, Metadata metadata) {
+		try {
+			System.out.println("Reading " + blockLength + " bytes of data");
+			ByteBuffer buffer = ByteBuffer.allocate(blockLength);
+			channel.read(buffer);
+			buffer.flip();
+
+			// steps taken directly from https://xiph.org/vorbis/doc/v-comment.html
+
+			// vorbis uses little endian
+			buffer.order(ByteOrder.LITTLE_ENDIAN);
+
+			// 1) [vendor_length] = read an unsigned integer of 32 bits
+			int vendor_length = buffer.getInt();
+
+			// 2) [vendor_string] = read a UTF-8 vector as [vendor_length] octets
+			// because vorbis uses utf-8, endianness shouldn't matter here
+			byte[] vendor_string = new byte[vendor_length];
+			buffer.get(vendor_string);
+
+			// 3) [user_comment_list_length] = read an unsigned integer of 32 bits
+			int user_comment_list_length = buffer.getInt();
+
+			// 4) iterate [user_comment_list_length] times {
+			for (int c = 0; c < user_comment_list_length; ++c) {
+				// 5) [length] = read an unsigned integer of 32 bits
+				int length = buffer.getInt();
+
+				// 6) this iteration's user comment = read a UTF-8 vector as [length] octets
+				byte[] comment = new byte[length];
+				buffer.get(comment);
+
+				int equalSign = -1;
+				for (int i = 0; i < length; ++i) {
+					if (comment[i] == '=') {
+						equalSign = i;
+						break;
+					}
+				}
+
+				if (equalSign != -1) {
+					String tag = new String(comment, 0, equalSign);
+					String value = new String(comment, equalSign + 1, length - equalSign - 1);
+
+					// convert vorbis tags to a descriptor common across all supported formats
+					if (VORBIS_TAGS.containsKey(tag)) {
+						tag = VORBIS_TAGS.get(tag);
+					}
+
+					metadata.addTextField(tag, value);
+				}
+			}
+
+			// steps 7-9 are not applicable to FLAC as framing_bit is not used
+
+			// explicit return to big endian even though buffer will be reassigned
+			buffer.order(ByteOrder.BIG_ENDIAN);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
